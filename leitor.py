93210493a06912e2a7ebc2269d7a407b81258a60
Antonio Vinicius Siqueira <46322262+vinizera@users.noreply.github.com>
2021-10-20 01:49:43 -0300

    Update leitor.py
    Lê cabeçalho separadamente (header);
    Organiza as linhas por listas;
    Altera nome de variáveis;

--- leitor.py
+++ leitor.py
@@ -1,4 +1,6 @@
-with open("frequencia.csv", "r", encoding='utf-8') as freq:
-    freq.readline()
-    for line in freq:
-        print(line.rstrip())
+with open("frequencia.csv", "r", encoding='utf-8') as file:
+    header = file.readline().rstrip()
+    print(header.split('\t'))
+    for row in file.readlines():
+        line = row.rstrip()
+        print(line.split('\t'))
