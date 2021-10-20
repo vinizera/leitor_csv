bb374c0033a5d68f94949dbb3d1b0f99149025cc
Vinicius Siqueira <vinicius_seek@yahoo.com>
2021-10-20 19:35:38 -0300

    Implementa biblioteca csv;

--- reader.py
+++ reader.py
@@ -1,14 +1,13 @@
+import csv
 from defs.gen import dictGen
-
 
 data = list()
 
 with open("frequencia.csv", "r", encoding='utf-8') as file:
-    header = file.readline().rstrip().split(',')
-    for row in file.readlines():
-        line = row.rstrip().split(',')
-        dictGen(line, data)
-
+    reader = csv.reader(file)
+    header = next(reader)
+    for row in reader:
+        dictGen(row, data)
 
 for i in data:
     print(i)
