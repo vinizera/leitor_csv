dec326be978b53e53c683192efc1d4926b2fd4e6
Vinicius Siqueira <vinicius_seek@yahoo.com>
2021-10-20 15:52:23 -0300

    Altera o para o padrão de vírgula;

--- reader.py
+++ reader.py
@@ -1,6 +1,6 @@
 with open("frequencia.csv", "r", encoding='utf-8') as file:
     header = file.readline().rstrip()
-    print(header.split('\t'))
+    print(header.split(','))
     for row in file.readlines():
-        line = row.rstrip().split('\t')
+        line = row.rstrip().split(',')
         print(line)
