290ad92feeb235b34b7f1c4cd600724691dbe4a8
Vinicius Siqueira <vinicius_seek@yahoo.com>
2021-10-20 15:07:51 -0300

    Cria função de nomeação de data; Apaga arquivos e pastas ambíguos;

---
+++ reader.py
@@ -0,0 +1,6 @@
+with open("frequencia.csv", "r", encoding='utf-8') as file:
+    header = file.readline().rstrip()
+    print(header.split('\t'))
+    for row in file.readlines():
+        line = row.rstrip().split('\t')
+        print(line)
