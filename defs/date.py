558c602def9719afefca0e9ff426bd5d1d6d20d0
Vinicius Siqueira <vinicius_seek@yahoo.com>
2021-10-20 19:41:25 -0300

    Comenta função getDateNum;

--- defs/date.py
+++ defs/date.py
@@ -1,4 +1,7 @@
 def getDateNum():
+    """
+    :return: Data atual numericamente formatada (sem quaisquer sinais)
+    """
     from datetime import date
     date_name = str(date.today()).replace("-", "")
     return date_name
