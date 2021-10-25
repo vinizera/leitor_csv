120fd025d0129c71976eec08bbe7bba423dc7168
Vinicius Siqueira <vinicius_seek@yahoo.com>
2021-10-25 10:30:41 -0300

    Atualiza leitor com validações

--- reader.py
+++ reader.py
@@ -1,13 +1,36 @@
 import csv
-from defs.gen import dictGen
+from defs.check import logCheck, checkTimeDif
 
+# Lista que abrigará todos os dicionários referentes a cada linha de frequência registrada:
 data = list()
 
+# Abertura do arquivo csv para leitura (file):
 with open("frequencia.csv", "r", encoding='utf-8') as file:
-    reader = csv.reader(file)
-    header = next(reader)
+    # Leitor do arquivo como dicionário (chaves = colunas; valores = campos);
+    # * a função csv.DictReader ignora a apresentação do cabeçalho e transforma seus itens em chaves automaticamente:
+    reader = csv.DictReader(file)
+    # Coleção de chaves válidas únicas geradas na função logCheck:
+    key_set = set()
+    # Início de laço para leitura das linhas csv:
     for row in reader:
-        dictGen(row, data)
+        id = row["id_usuario"]
+        initial_dat = row["data_inicio"]
+        final_dat = row["data_fim"]
+        initial_time = row["hora_inicio"]
+        final_time = row["hora_fim"]
+        # Validação de log usando id (id_usuário), datas(inicio e fim) com a coleção de chaves válidas:
+        if logCheck(id, initial_dat, final_dat, key_set):
+            # Validação de datas comparando as entradas das datas de início e fim:
+            if checkTimeDif(initial_time, final_time):
+                # O retorno de confirmação (True) adiciona a linha(dicionário) na lista <data>:
+                data.append(row)
+            else:
+                # O retorno de negação (False) da função <checkTimeDif> ignora a linha:
+                continue
+        else:
+            # O retorno de negação (False) da função <logCheck> ignora a linha:
+            continue
+
 
 for i in data:
     print(i)
