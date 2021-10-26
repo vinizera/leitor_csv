710444cac3edc49299ec622e2b46a3f6f52b49fc
Marcello Fiusa <marcellofiusa@gmail.com>
2021-10-26 10:46:18 -0300

    Atualização de lista com erros

--- reader.py
+++ reader.py
@@ -1,8 +1,11 @@
 import csv
-from defs.check import logCheck, checkTimeDif
+from defs.check import logCheck, checkTimeDif, calculateTimeDif
 
 # Lista que abrigará todos os dicionários referentes a cada linha de frequência registrada:
 data = list()
+wrong_data = list()
+
+
 
 # Abertura do arquivo csv para leitura (file):
 with open("frequencia.csv", "r", encoding='utf-8') as file:
@@ -18,6 +21,7 @@ with open("frequencia.csv", "r", encoding='utf-8') as file:
         final_dat = row["data_fim"]
         initial_time = row["hora_inicio"]
         final_time = row["hora_fim"]
+        work_bank = row["banco_horas"]
         # Validação de log usando id (id_usuário), datas(inicio e fim) com a coleção de chaves válidas:
         if logCheck(id, initial_dat, final_dat, key_set):
             # Validação de datas comparando as entradas das datas de início e fim:
@@ -26,11 +30,18 @@ with open("frequencia.csv", "r", encoding='utf-8') as file:
                 data.append(row)
             else:
                 # O retorno de negação (False) da função <checkTimeDif> ignora a linha:
-                continue
+                wrong_data.append(row)
         else:
             # O retorno de negação (False) da função <logCheck> ignora a linha:
-            continue
+            wrong_data.append(row)
 
-
+print("\n----------------------------------------------Entradas Corretas----------------------------------------------")
 for i in data:
     print(i)
+
+print("\n----------------------------------------------Entradas Erradas----------------------------------------------")
+
+for i in wrong_data:
+    print(i)
+
+
